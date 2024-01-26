98b6826e4a99e6098014675b98468bd27d4acafc
sanjith <sanjith1309@gmail.com>
2024-01-26 22:01:42 +0530

    sanjith

--- lib/garbagetruck/truckhome.dart
+++ lib/garbagetruck/truckhome.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
@@ -10,6 +12,7 @@ class truckhome extends StatefulWidget {
 }
 
 class _truckhomeState extends State<truckhome> {
+  bool isFiltering = false;
   List<String> fcmTokens = [];
 
   double? _latitude;
@@ -84,12 +87,18 @@ class _truckhomeState extends State<truckhome> {
             },
             child: Text("abcd"),
           ),
+          SizedBox(height: 10), // Adjust the spacing as needed
+          if (isFiltering) // Show circular progress indicator if isFiltering is true
+            CircularProgressIndicator(),
         ],
       )),
     ));
   }
 
-  void filter() {
+  Future<void> filter() async {
+    setState(() {
+      isFiltering = true;
+    });
     final databaseReference = FirebaseDatabase.instance.reference();
     databaseReference.child('wastemanagement').once().then((event) {
       if (event.snapshot.value != null) {
@@ -100,16 +109,61 @@ class _truckhomeState extends State<truckhome> {
           double latitude = value['latitude'];
           double longitude = value['longitude'];
           String fcmtoken = value['fcmtoken'];
-
           if (isWithin1Km(latitude, longitude)) {
             // This device is within the 1 km radius
-            if(!fcmTokens.contains(fcmtoken))
-            fcmTokens.add(fcmtoken);
+            if (!fcmTokens.contains(fcmtoken)) fcmTokens.add(fcmtoken);
           }
         });
 
         print(fcmTokens);
       }
+    });
+
+    final serverKey =
+        'AAAAlkd6_zo:APA91bGYuaF6MVjfQP1RT51rTHj8YvXWbstr9g3ITjvlwzX-tK8nYM6VFADaLrm_V6kazWUvmYRZlXybshtQFBud1Q0OnBv2_k5ZYfOvQeqBfgIXpqwBYKWdB6mYSewi-oPFMmBFPWX7';
+    final url = Uri.parse('https://fcm.googleapis.com/fcm/send');
+    final headers = <String, String>{
+      'Content-Type': 'application/json',
+      'Authorization': 'key=$serverKey',
+    };
+
+// Retrieve all tokens from the database
+    final databaseRefer = FirebaseDatabase.instance.reference();
+    final event = await databaseRefer.child('usertokens').once();
+    final snapshot = event.snapshot;
+
+    if (snapshot.value != null) {
+      final tokens =
+          Map<String, dynamic>.from(snapshot.value as Map<dynamic, dynamic>);
+
+      // Send a notification to each token
+      for (final token in fcmTokens) {
+        final body = jsonEncode(<String, dynamic>{
+          'notification': <String, dynamic>{
+            'body': 'The Garbage truck is near by...with 1 KM',
+            'title': 'Garbage truck notification',
+          },
+          'priority': 'high',
+          'data': <String, dynamic>{
+            'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+            'id': '1',
+            'status': 'done',
+            // Change this to the appropriate sound value
+          },
+          'to': token,
+        });
+
+        final response = await http.post(url, headers: headers, body: body);
+
+        if (response.statusCode == 200) {
+          print('Notification sent successfully to $token');
+        } else {
+          print('Notification not sent to $token');
+        }
+      }
+    }
+    setState(() {
+      isFiltering = false;
     });
   }
 
@@ -120,8 +174,6 @@ class _truckhomeState extends State<truckhome> {
       latitude,
       longitude,
     );
-
-    // Check if the distance is less than 1 km (adjust this value as needed)
     return distance <= 1000;
   }
 }
