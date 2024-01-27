c79e0be8088382fa6685e0694a218a3d7e36d5cb
sanjith <sanjith1309@gmail.com>
2024-01-27 16:08:48 +0530

    sanjith

--- lib/screens/menupage.dart
+++ lib/screens/menupage.dart
@@ -1,9 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:city_app/nearbylocationmaps/nearbymap.dart';
 import 'package:city_app/screens/alerts.dart';
 import 'package:city_app/screens/chat.dart';
 import 'package:city_app/screens/events.dart';
 import 'package:city_app/screens/news.dart';
 import 'package:city_app/screens/pressrelease.dart';
+import 'package:city_app/screens/shop.dart';
+import 'package:city_app/screens/social.dart';
 import 'package:city_app/screens/sos.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -20,13 +23,32 @@ class menupage extends StatefulWidget {
 
 class _menupageState extends State<menupage> {
   String abcd = '';
-
+  final dat = FirebaseDatabase.instance.reference();
+  List<String> imageUrls = [];
   double? _latitude;
   double? _longitude;
 
   void initState() {
     super.initState();
     _loadMarkersFromDatabase();
+    _fetchImageUrls();
+  }
+
+  void _fetchImageUrls() async {
+    DatabaseEvent event = await dat.child('photosevents').once();
+    DataSnapshot dataSnapshot = event.snapshot;
+
+    if (dataSnapshot.value != null && dataSnapshot.value is List<Object?>) {
+      List<Object?> imageList = dataSnapshot.value as List<Object?>;
+
+      // Filter out null values and convert to strings
+      imageUrls = imageList
+          .where((element) => element != null)
+          .map((element) => element.toString())
+          .toList();
+    }
+
+    print(imageUrls);
   }
 
   Future<void> _loadMarkersFromDatabase() async {
@@ -59,10 +81,10 @@ class _menupageState extends State<menupage> {
       print(e);
     }
     final databaseReference = FirebaseDatabase.instance.reference();
-    databaseReference.child('livelocations/${widget.username}').set({
-      'latitude': _latitude,
-      'longitude': _longitude,
-    });
+    // databaseReference.child('livelocations/${widget.username}').set({
+    //   'latitude': _latitude,
+    //   'longitude': _longitude,
+    // });
     databaseReference.child('wastemanagement/${widget.username}').set({
       'latitude': _latitude,
       'longitude': _longitude,
@@ -152,61 +174,93 @@ class _menupageState extends State<menupage> {
                       left: screenWidth * 0.05, right: screenWidth * 0.05),
                   child: Column(
                     children: [
+                      // const SizedBox(
+                      //   height: 10,
+                      // ),
+                      // Row(
+                      //   children: [
+                      //     InkWell(
+                      //       onLongPress: sospressed,
+                      //       child: Container(
+                      //         height: screenWidth * 0.4,
+                      //         width: screenWidth * 0.4,
+                      //         decoration: BoxDecoration(
+                      //             borderRadius: BorderRadius.circular(40),
+                      //             gradient: const RadialGradient(colors: [
+                      //               Colors.red,
+                      //               Color.fromARGB(255, 205, 37, 25),
+                      //               Color.fromARGB(255, 156, 1, 1)
+                      //             ])),
+                      //         child: const Stack(children: [
+                      //           Align(
+                      //             alignment: Alignment.center,
+                      //             child: Text(
+                      //               "SOS",
+                      //               style: TextStyle(
+                      //                   color: Colors.white, fontSize: 50),
+                      //             ),
+                      //           ),
+                      //         ]),
+                      //       ),
+                      //     ),
+                      //     const SizedBox(
+                      //       width: 10,
+                      //     ),
+                      //     const Expanded(
+                      //       child: Text.rich(
+                      //         TextSpan(
+                      //           text:
+                      //               "Use this SOS feature responsibly and only in life-threatening situations. ",
+                      //           style: TextStyle(fontSize: 18),
+                      //           children: <TextSpan>[
+                      //             TextSpan(
+                      //               text: "Long press to activate.",
+                      //               style: TextStyle(
+                      //                   fontSize: 20,
+                      //                   fontWeight: FontWeight.bold,
+                      //                   color: Color.fromARGB(255, 226, 23, 9)),
+                      //             ),
+                      //           ],
+                      //         ),
+                      //         maxLines: 7,
+                      //       ),
+                      //     ),
+                      //   ],
+                      // ),
                       const SizedBox(
                         height: 10,
                       ),
-                      Row(
-                        children: [
-                          InkWell(
-                            onLongPress: sospressed,
-                            child: Container(
-                              height: screenWidth * 0.4,
-                              width: screenWidth * 0.4,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(40),
-                                  gradient: const RadialGradient(colors: [
-                                    Colors.red,
-                                    Color.fromARGB(255, 205, 37, 25),
-                                    Color.fromARGB(255, 156, 1, 1)
-                                  ])),
-                              child: const Stack(children: [
-                                Align(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    "SOS",
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 50),
+                      CarouselSlider(
+                        options: CarouselOptions(
+                          autoPlay: true,
+                        ),
+                        items: imageUrls.map((url) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Container(
+                                width: MediaQuery.of(context).size.width,
+                                height: 200,
+                                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  border: Border.all(
+                                      color: Colors.black, width: 2.0),
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  child: Image.network(
+                                    url,
+                                    fit: BoxFit
+                                        .fill, // Change fit property to contain
                                   ),
                                 ),
-                              ]),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          const Expanded(
-                            child: Text.rich(
-                              TextSpan(
-                                text:
-                                    "Use this SOS feature responsibly and only in life-threatening situations. ",
-                                style: TextStyle(fontSize: 18),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                    text: "Long press to activate.",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color.fromARGB(255, 226, 23, 9)),
-                                  ),
-                                ],
-                              ),
-                              maxLines: 7,
-                            ),
-                          ),
-                        ],
+                              );
+                            },
+                          );
+                        }).toList(),
                       ),
                       const SizedBox(
-                        height: 15,
+                        height: 10,
                       ),
                       Row(
                         children: [
@@ -226,7 +280,7 @@ class _menupageState extends State<menupage> {
                         ],
                       ),
                       const SizedBox(
-                        height: 15,
+                        height: 10,
                       ),
                       Row(
                         children: [
@@ -246,7 +300,7 @@ class _menupageState extends State<menupage> {
                         ],
                       ),
                       const SizedBox(
-                        height: 15,
+                        height: 10,
                       ),
                       Row(
                         children: [
@@ -265,30 +319,22 @@ class _menupageState extends State<menupage> {
                               Icons.public_sharp),
                         ],
                       ),
-                      // const SizedBox(
-                      //   height: 15,
-                      // ),
-                      // Row(
-                      //   children: [
-                      //     buildContainer(
-                      //         screenWidth * 0.55,
-                      //         screenHeight * 0.15,
-                      //         'Precautions',
-                      //         Precautions(),
-                      //         Icons.local_hospital_outlined),
-                      //     Spacer(),
-                      //     buildContainer(
-                      //         screenWidth * 0.30,
-                      //         screenHeight * 0.15,
-                      //         'Emergency Contact',
-                      //         emergency(),
-                      //         Icons.emergency_outlined),
-                      //   ],
-                      //),
-                      const SizedBox(
-                        height: 15,
-                      ),
                       
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          bbbb(screenWidth * 0.68, screenHeight * 0.08,
+                              'Social', social(), Icons.eco),
+                          Spacer(),
+                          bbbb(screenWidth * 0.18, screenHeight * 0.08, '',
+                              shop(), Icons.shopping_bag_outlined),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
                     ],
                   ),
                 ),
@@ -316,6 +362,62 @@ class _menupageState extends State<menupage> {
               Color.fromARGB(255, 0, 0, 0),
               Color.fromARGB(255, 11, 51, 83),
               Colors.blue,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  text,
+                  style: const TextStyle(color: Colors.white, fontSize: 19),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  icon,
+                  size: 55,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget bbbb(
+      double width, double height, String text, Widget screen, IconData icon) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => screen),
+        );
+      },
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: const LinearGradient(
+            colors: [
+              Color.fromARGB(255, 6, 73, 29),
+              Color.fromARGB(255, 65, 187, 27),
+              // Color.fromARGB(255, 83, 252, 36),
+              Color.fromARGB(255, 36, 223, 252),
             ],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
---
+++ lib/screens/social.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class social extends StatefulWidget {
+  const social({super.key});
+
+  @override
+  State<social> createState() => _socialState();
+}
+
+class _socialState extends State<social> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
