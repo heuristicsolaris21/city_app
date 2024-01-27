fe29777328748d098497bd19740d4bad0725c943
sanjith <sanjith1309@gmail.com>
2024-01-27 07:34:28 +0530

    sanjith

--- lib/others/newscard.dart
+++ lib/others/newscard.dart
@@ -17,8 +17,10 @@ class newscard extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Card(
+Widget build(BuildContext context) {
+  return Padding(
+    padding: EdgeInsets.only(left: 20,right: 20,top: 10),
+    child: Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
       ),
@@ -27,18 +29,44 @@ class newscard extends StatelessWidget {
         borderRadius: BorderRadius.circular(15.0),
         onTap: _launchURL,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             ClipRRect(
               borderRadius: BorderRadius.vertical(top: Radius.circular(15.0)),
-              child: Image.network(image),
+              child: Image.network(
+                image,
+                height: 150, // Adjust the height as needed
+                fit: BoxFit.cover,
+              ),
             ),
-            ListTile(
-              title: Text(title),
-              subtitle: Text(desc),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    desc,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-    );
-  }
+    ),
+  );
+}
+
 }
